ce2f1b4fbc7966cdeb8a1d7a96837bddf8a9cc76
Ashiqur Rahman Naeem <ashiqur47@student.sust.edu>
2022-08-26 03:33:15 +0600

    Added system notification support using plyer

--- battery_alert.py
+++ battery_alert.py
@@ -1,6 +1,7 @@
 import time
 import psutil
 import os
+from plyer import notification
 while True:
 	battery = psutil.sensors_battery()
 	if battery.power_plugged == False:
@@ -8,6 +9,7 @@ while True:
 		break
 	if battery.percent >= 75:
 		print("battery.percent: ", battery.percent, "%. Plug out the power calbe !!!")
+		notification.notify(title="Battery Alert", message="Battery Charged Over: "+str(battery.percent)+"%. Unplug immediately!!!")
 		os.system("msg * Battery Charge: " + str(battery.percent) + "% !!! Disconnect the power cable")
 		break
 	else:
